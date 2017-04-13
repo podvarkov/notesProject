7066b44a4e0f3afc9e346790f10783c5814e0f98
podvarkov <i.snova.bebebe@gmail.com>
2017-04-13 15:17:56 +0500

    new class

---
+++ app/src/main/java/com/example/max/myapplication/TestClassGit.java
@@ -0,0 +1,13 @@
+package com.example.max.myapplication;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by MAX on 13.04.2017.
+ */
+public class TestClassGit extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
